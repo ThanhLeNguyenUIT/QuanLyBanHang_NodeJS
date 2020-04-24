8c464d797efb6a7b7fbc25cc3fd30bc2a28f3be9
WayciQ <hoangquang0511@gmail.com>
2020-04-24 20:33:17 +0700

    collape css

---
+++ resource/js/hanghoa.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    var category_val;
+    $(".btn-add").click(function() {
+        $(".modal_add-product").css("display", "flex");
+        $(".auth-form").css("display", "block");
+    });
+
+    $(".btn-product-cancel").click(function() {
+        $(".modal_add-product").css("display", "none");
+        $(".auth-form").css("display", "none");
+    });
+    $(".add-category,.auth-form_plus-icon").click(function() {
+        $(".modal-category").css("display", "flex");
+        $(".auth-form_min-add").css("display", "block");
+    });
+    $(".btn-add-category-cancel").click(function() {
+        $(".modal-category").css("display", "none");
+        $(".auth-form_min-add").css("display", "none");
+    });
+    $(".category-item-icon").click(function() {
+        $(".modal-category").css("display", "flex");
+        $(".auth-form_min-edit").css("display", "block");
+    });
+    $(".btn-edit-category-cancel").click(function() {
+        $(".modal-category").css("display", "none");
+        $(".auth-form_min-edit").css("display", "none");
+    });
+    $(".auth-form_wrap.auth-form_input-group-product,.auth-form_wrap.auth-form_arrow-icon").click(function() {
+        $(".category-Product-list").slideDown();
+    })
+    $(".auth-form_wrap.category-Product-item").click(function() {
+        var temp = $(this).text();
+        $(".auth-form_input-group-product").val(temp);
+        $(".category-Product-list").slideUp();
+    })
+
+})
