d3a74e67f025fa355f1181f1314202e72daf690b
WayciQ <hoangquang0511@gmail.com>
2020-05-01 15:51:41 +0700

    day du

--- resource/js/baocao.js
+++ resource/js/baocao.js
@@ -1,22 +1,4 @@
 $(function() {
-    $("#reportContainer").dxDataGrid({
-        dataSource: customers,
-        showBorders: true,
-        paging: {
-            pageSize: 10
-        },
-        pager: {
-            showPageSizeSelector: true,
-            allowedPageSizes: [5, 10, 20],
-            showInfo: true
-        },
-
-        columns: ["STT", "Agency", "so phieu xuat", "num", "Title"]
-
-    });
-
-});
-$(function() {
     var selectedDateFrom = $("#selected-date-from").dxDateBox({
         value: new Date(),
         width: "100%",
@@ -40,10 +22,3 @@ $(function() {
 
 })
 var selections = ["Báo cáo", "Công nợ"];
-var customers = [{
-    "STT": 1,
-    "Agency": "Premier Buy",
-    "so phieu xuat": 14,
-    "num": 10000000,
-    "Title": 3.5
-}];
