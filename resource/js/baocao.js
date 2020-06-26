5ea641a767c0d66f775494fe164ed3987adace05
WayciQ <hoangquang0511@gmail.com>
2020-06-26 17:52:10 +0700

    con moi chia file

--- resource/js/baocao.js
+++ resource/js/baocao.js
@@ -13,28 +13,35 @@ $(document).ready(function() {
             calendar.option("value", data.value);
         }
     }).dxDateBox("instance");
-
     var radioGroup = $("#radio-group-with-selection").dxRadioGroup({
         items: selections,
-        onValueChanged: function(e) {
-            $each(task, function(i, item) {
-                if (item.selection == e.value) {
-                    $("#report-table_header-title").text(task[i].title);
+        onOptionChanged: function(e) {
+            $.each(task, function(i, item) {
+                if (item.selection === e.value) {
+                    $('#report_CN_DT').show();
+                    $('#report_HD').hide();
+                    $(".report-table_header-title").text(task[i].title);
                     $("#column_2-header").text(task[i].column_2);
                     $("#column_3-header").text(task[i].column_3);
                     $("#column_4-header").text(task[i].column_4);
                 }
             })
+            if (e.value == "Hoá đơn") {
+                $('#report_HD').show();
+                $('#report_CN_DT').hide();
+
+            }
         }
+
     }).dxRadioGroup("instance");
-    radioGroup.option("value", selections[1]);
+    radioGroup.option("value", selections[0]);
 
     $(".view-bill-btn").click(function() {
-        $(".modal_bill-details").css("display","flex");
+        $(".modal_bill-details").css("display", "flex");
         $(".auth-form").css("display", "block");
     })
     $(".btn-cancel").click(() => {
-        $(".modal_bill-details").css("display","none");
+        $(".modal_bill-details").css("display", "none");
         $(".auth-form").css("display", "none");
     })
 })
@@ -50,5 +57,5 @@ var task = [{
     column_2: "Nợ Đầu",
     column_3: "Phát Sinh",
     column_4: "Nợ cuối",
-    selection: "Công nợ"
+    selection: "Công nợ"
 }]
--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,98 @@
 var express = require('express');
 var router = express.Router();
-
-router.get('/account',(req,res) => {
-    res.render('account');
+var User = require('../model/user');
+let getErrors = '';
+router.post('/searchAccount', (req, res) => {
+    User.find({ 'email': req.body.searchAccount }).then((data) => {
+        console.log(data)
+        res.render('account', { acc: data, errors: null, message: null });
+    })
 })
+
+router.get('/accountAdmin', function(req, res) {
+
+    User.find({ 'category_account': 'Admin' }).sort({ name: 1 }).then((data) => {
+        res.render('account', { acc: data, errors: null, message: null })
+    })
+})
+router.get('/accountNV', function(req, res) {
+
+    User.find({ 'category_account': 'Nhân viên' }).sort({ name: 1 }).then((data) => {
+        res.render('account', { acc: data, errors: null, message: null })
+    })
+
+})
+
+router.get('/taikhoan', (req, res) => {
+    User.find().sort({ name: 1 }).then((data) => {
+        res.render('account', { acc: data, errors: getErrors, message: req.flash('message') })
+    })
+})
+
+router.post('/taikhoan/them-taikhoan', async(req, res) => {
+    req.checkBody('email', 'Vui lòng nhập Email').isEmail();
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) {
+        req.checkBody('email', 'Đã tồn tại Email').not().equals(emailExist.email);
+    }
+    getErrors = req.validationErrors();
+    if (getErrors) {
+        res.redirect('/taikhoan')
+    } else {
+        const account = new User({
+            name: req.body.name_account,
+            email: req.body.email,
+            password: req.body.password,
+            category_account: req.body.category_account,
+            sexual: req.body.sexual,
+            address_account: req.body.address_account,
+            phone_account: req.body.phone_account,
+            date: req.body.dateBegin,
+            salary: req.body.salary
+        })
+        account.save().then(function() {
+            req.flash('message', "thêm tài khoản thành công");
+            res.redirect('/taikhoan')
+        })
+    }
+})
+
+router.get("/taikhoan/xoa-taikhoan/:id", function(req, res) {
+    User.findByIdAndDelete(req.params.id).then(function() {
+        req.flash('message', "xoá sản phẩm thành công ");
+        res.redirect('/taikhoan');
+    })
+})
+
+router.get('/taikhoan/sua-taikhoan/:id', (req, res) => {
+    User.findById(req.params.id).then(function(data) {
+        res.send(data);
+    })
+})
+
+router.post('/taikhoan/sua-taikhoan/:id', (req, res) => {
+
+    getError = req.validationErrors();
+    if (getError) {
+        console.log(getError)
+        res.redirect('/taikhoan');
+    } else {
+        var acc = ({
+            'name': req.body.name_account,
+            'password': req.body.password,
+            'category_account': req.body.category_account,
+            'sexual': req.body.sexual,
+            'address_account': req.body.address_account,
+            'phone_account': req.body.phone_account,
+            'date': req.body.dateBegin,
+            'salary': req.body.salary
+        })
+        User.findByIdAndUpdate(req.params.id, acc, { new: true, upsert: true }).then(() => {
+            req.flash('message', "Sửa tài khoản thành công");
+            res.redirect('/taikhoan');
+        });
+    }
+})
+
 
 module.exports = router;
